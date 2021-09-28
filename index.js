c409fa2752efa1bb6766bec6d90a8c3bb71bafee
Bmitang <86649939+Bmitang@users.noreply.github.com>
2021-09-28 15:58:25 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+function randomDicee(Dicee) {
+    var num = Math.floor(Math.random() * (6) + 1)
+    var randomDiceImage = "dice" + num +".png"; //dice1.png - dice6.png
+    var randomImageSource = "images/" + randomDiceImage; // images/dice1.png - 
+    var imgae = document.querySelectorAll("img")[Dicee]
+    imgae.setAttribute("src",randomImageSource)
+
+    return num
+}
+let num1 = randomDicee(0);
+let num2 = randomDicee(1);
+let num3 = randomDicee(2);
+
+
+// if num1 > num2 say Play 1 Wins!
+// if num2 > num1 say Play 2 Wins!
+// if num2 = num1 say Draw!
+
+if (num1 > num2 && num1 > num3) {
+    document.querySelector("h1").innerHTML = "Play 1 Wins!"
+}
+else if (num2 > num1 && num2 > num3) {
+    document.querySelector("h1").innerHTML = "Play 2 Wins!"
+}
+else if (num3 > num1 && num3 > num2) {
+    document.querySelector("h1").innerHTML = "Play 3 Wins!"
+}
+else if (num1 > num3 && num2 > num3 && num1 == num2) {
+    document.querySelector("h1").innerHTML = "Play 1 , 2 Wins!"
+}
+else if (num1 > num2 && num3 > num2 && num1 == num3) {
+    document.querySelector("h1").innerHTML = "Play 1 , 3 Wins!"
+}
+else if (num2 > num1 && num3 > num1 && num2 == num3) {
+    document.querySelector("h1").innerHTML = "Play 2 , 3 Wins!"
+} else {
+    document.querySelector("h1").innerHTML = "Draw"
+}
